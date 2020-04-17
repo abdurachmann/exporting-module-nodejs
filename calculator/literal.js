375646d84e39c39b6c45deddf9e5d66368c70d63
abdurachmann <abrchmn@gmail.com>
2020-04-17 16:30:35 +0700

    first commit

---
+++ calculator/literal.js
@@ -0,0 +1,13 @@
+let angka1 = 9;
+let angka2 = 6;
+
+// Rules
+
+const deklarasi = 'angka pertama adalah = ' + angka1 + '\nangka kedua adalah = ' + angka2;
+const penjumlahan = '\nhasil penjumlahan dari angka diatas = ' +(angka1+angka2);
+const pengurangan = '\nhasil pengurangan dari angka diatas = ' +(angka1-angka2);
+const perkalian = '\nhasil perkalian dari angka diatas = ' +(angka1*angka2);
+const pembagian = '\nhasil pembagian dari angka diatas = ' +(angka1/angka2);
+
+
+module.exports =  deklarasi + penjumlahan + pengurangan + perkalian + pembagian;
